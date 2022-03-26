1e4eb3345dd83177d07621c269cecd3106e2e6ef
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-03-26 11:04:25 -0600

    update

--- Scripts/PlayerScripts/animation.cs
+++ Scripts/PlayerScripts/animation.cs
@@ -1,58 +1,95 @@
-//! DEVELOPMENT
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+// Takes and handles input and movement for a player character
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    
 
- //! PRODUCTION
+    Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+    Animator animator;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
+    bool canMove = true;
 
- 
-public class PlayerMovement : MonoBehaviour{
-
-
-public float moveSpeed;
-
-public Rigidbody2D rb;
-
-// Animation controls start
-Animator animator;
-
-Vector2 movement;
-
-
-   
-
- void Start() {
-     animator = GetComponent<Animator>();
- }
-    void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        MovementInput();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void FixedUpdate() {
-        rb.velocity = movement * moveSpeed;
-        if(movement != Vector2.zero) {
-            bool success = TryMove(movement);
-            if(!success) {
-                success = TryMove(new Vector2(movement.x, 0));
+        if(canMove) {
+            // If movement input is not 0, try to move
+            if(movementInput != Vector2.zero){
+                
+                bool success = TryMove(movementInput);
+
                 if(!success) {
-                    success = TryMove(new Vector2(0, movement.y));
+                    success = TryMove(new Vector2(movementInput.x, 0));
                 }
+
+                if(!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+                
+                animator.SetBool("isMoving", success);
+            } else {
+                animator.SetBool("isMoving", false);
             }
-            animator.SetBool("isMoving", success);
-        }else {
-            animator.SetBool("isMoving", false);
+
+            // Set direction of sprite to movement direction
+            if(movementInput.x < 0) {
+                spriteRenderer.flipX = true;
+            } else if (movementInput.x > 0) {
+                spriteRenderer.flipX = false;
+            }
         }
-
     }
-    void MovementInput() {
-        float mx = Input.GetAxisRaw("Horizontal");
-        float my = Input.GetAxisRaw("Vertical");
-        
-        movement = new Vector2(mx, my).normalized;
 
+    private bool TryMove(Vector2 direction) {
+        if(direction != Vector2.zero) {
+            // Check for potential collisions
+            int count = rb.Cast(
+                direction, 
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+            if(count == 0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            // Can't move if there's no direction to move in
+            return false;
+        }
+        
+    }
+
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+
+
+
+    public void LockMovement() {
+        canMove = false;
+    }
+
+    public void UnlockMovement() {
+        canMove = true;
     }
 }
